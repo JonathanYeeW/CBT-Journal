7f78de2c49c9b5fa535e81b812a8fe1c3346b7ef
Jonathan Yee <Jonathan.Yee.W@gmail.com>
2018-05-27 16:48:31 -0700

    Sunday May 27, 2018

--- CBT Journal/View Controllers/MainTVC.swift
+++ CBT Journal/View Controllers/MainTVC.swift
@@ -17,6 +17,7 @@ class MainTVC: UITableViewController {
             dayManager.createDay()
         }
         ReloadTableView()
+        triggerAddButton(false)
     }//End
     
     override func didReceiveMemoryWarning() {
@@ -30,12 +31,15 @@ class MainTVC: UITableViewController {
     
 // ======================= Outlets =======================
     
+    @IBOutlet weak var addButtonLabel: UIBarButtonItem!
+    
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         print("@@ addButtonPressed @@")
         if dayManager.checkIfTodayAlreadyCreated() == false {
             dayManager.createDay()
         }
         ReloadTableView()
+        triggerAddButton(false)
     }
     // ======================= TableView Functions =======================
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -69,6 +73,7 @@ class MainTVC: UITableViewController {
         let dayObject = dayArray[indexPath.row]
         dayManager.deleteSingleDay(dayObject: dayObject)
         ReloadTableView()
+        triggerAddButton(true)
     }//End
     
 // ======================= Functions =======================
@@ -78,4 +83,14 @@ class MainTVC: UITableViewController {
         dayArray = dayArray.sorted(by: {$0.createdAt! > $1.createdAt!})
         tableView.reloadData()
     }//End ReloadTableView()
+    
+    func triggerAddButton(_ setting: Bool){
+        if setting == true {
+            addButtonLabel.isEnabled = true
+            addButtonLabel.tintColor = UIColor.blue
+        } else {
+            addButtonLabel.isEnabled = false
+            addButtonLabel.tintColor = UIColor.clear
+        }
+    }
 }//End Class
